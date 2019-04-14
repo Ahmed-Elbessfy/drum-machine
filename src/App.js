f0aecdd5a0ebf25f9e60c14c9a2b174952299ccf
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-04-14 05:13:01 +0200

    add constructor and buttons data in state of App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,69 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      display:'',
+      active:true,
+      btnsData : [
+        {
+          id : 'Q',
+          key : 81,
+          sound : 'Heater-1',
+          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+        },
+        {
+          id : 'W',
+          key : 87,
+          sound : 'Heater-2',
+          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+        },
+        {
+          id : 'E',
+          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+          key : 69,
+          sound : 'Heater-3',
+        },
+        {
+          id : 'A',
+          key : 65,
+          sound : 'Shaker',
+          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+        },
+        {
+          id : 'S',
+          key : 83,
+          sound : 'Clap',
+          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+        },
+        {
+          id : 'D',
+          key : 68,
+          sound : 'Open-HH',
+          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+        },
+        {
+          id : 'Z',
+          key : 90,
+          sound : "Kick-n'-Hat",
+          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+        },
+        {
+          id : 'X',
+          key : 88,
+          sound : 'Kick',
+          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+        },
+        {
+          id : 'C',
+          key : 67,
+          sound : 'Closed-HH',
+          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+        },
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
