1352c3bf00c1fd7823e45903ff7d19992b24e862
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-04-15 11:55:52 +0200

    add Buttons Component content
    - map through props to create Buttons and audio elements
    - add functionality by addingg playSound and handleClick functions and eventListenr in componentDidMount function

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,17 +3,53 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Buttons extends Component {
+  // play sounds function
+  playAudio = (audio) => {
+    // setting audio time to 0 for multiple fast clicks
+    audio.currentTime = 0
+    // play audio
+    audio.play()
+  }
+  // onClick function
+  handleClick = (e) => {
+    // get audio child element from clicked button
+    let audio = e.target.children[0]
+    // call play sound function
+    this.playAudio(audio)
+  }
+
+  componentDidMount(){
+    // add event listener for keyboard clicks
+    document.addEventListener('keydown', (e) => {
+      // add event listener only if the clicked button is within a set of buttons
+      if ([81,87,69,65,83,68,90,88,67].indexOf(e.keyCode) !== -1){
+        //get audio with id equals to matching letter clicked on keyboard
+        let audio = document.querySelector(`#${String.fromCharCode(e.keyCode)}`)
+        this.playAudio(audio)
+      }
+    })
+  }
 
   render() {
     return (
       <div>
+        {this.props.btnsData.map(btn => {
+            return (
+              <button  key={btn.key} className='drum-pad' id={btn.sound} onClick={this.handleClick}>
+                {btn.id}
+                <audio src={btn.audioSrc} className='clip' id={btn.id} />
+              </button>
+            )
+          }
+        )}
       </div>
     );
   }
 }
 
 Buttons.propTypes = {
-
+  btnsData : PropTypes.array.isRequired
 };
 
 export default Buttons
+
