7a5045d388d4b2faaaf883391f1c9909083e045f
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-04-16 09:57:12 +0200

    Updates

---
+++ precache-manifest.fc979689cbe78cf887a54b4fc3c5a3c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afcf9fe73353caf758cf",
+    "url": "/drum-machine/static/js/runtime~main.27309cc5.js"
+  },
+  {
+    "revision": "7862b55d2b2d4656fc89",
+    "url": "/drum-machine/static/js/main.1eae58e1.chunk.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/drum-machine/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "7862b55d2b2d4656fc89",
+    "url": "/drum-machine/static/css/main.e65efe41.chunk.css"
+  },
+  {
+    "revision": "c464a86ef283d35a8c94767d480c7492",
+    "url": "/drum-machine/index.html"
+  }
+];
