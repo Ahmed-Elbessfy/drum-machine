c6faaa137c0f5ea212a7e5db5a53d5a91433134d
Ahmed-Elbessfy <ahmedelbessfy@gmail.com>
2019-04-16 09:43:08 +0200

    beautify code
    - use document format on App.js, Buttons.js and Index.css files

--- src/App.js
+++ src/App.js
@@ -1,100 +1,108 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Buttons from './components/Buttons'
+import Buttons from "./components/Buttons";
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      display:'',
-      active:true,
-      btnsData : [
+    this.state = {
+      display: "",
+      active: true,
+      btnsData: [
         {
-          id : 'Q',
-          key : 81,
-          sound : 'Heater-1',
-          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+          id: "Q",
+          key: 81,
+          sound: "Heater-1",
+          audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
         },
         {
-          id : 'W',
-          key : 87,
-          sound : 'Heater-2',
-          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+          id: "W",
+          key: 87,
+          sound: "Heater-2",
+          audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
         },
         {
-          id : 'E',
-          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
-          key : 69,
-          sound : 'Heater-3',
+          id: "E",
+          audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+          key: 69,
+          sound: "Heater-3"
         },
         {
-          id : 'A',
-          key : 65,
-          sound : 'Shaker',
-          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3',
+          id: "A",
+          key: 65,
+          sound: "Shaker",
+          audioSrc:
+            "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"
         },
         {
-          id : 'S',
-          key : 83,
-          sound : 'Clap',
-          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+          id: "S",
+          key: 83,
+          sound: "Clap",
+          audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
         },
         {
-          id : 'D',
-          key : 68,
-          sound : 'Open-HH',
-          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+          id: "D",
+          key: 68,
+          sound: "Open-HH",
+          audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
         },
         {
-          id : 'Z',
-          key : 90,
-          sound : "Kick-n'-Hat",
-          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+          id: "Z",
+          key: 90,
+          sound: "Kick-n'-Hat",
+          audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
         },
         {
-          id : 'X',
-          key : 88,
-          sound : 'Kick',
-          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+          id: "X",
+          key: 88,
+          sound: "Kick",
+          audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
         },
         {
-          id : 'C',
-          key : 67,
-          sound : 'Closed-HH',
-          audioSrc : 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
-        },
+          id: "C",
+          key: 67,
+          sound: "Closed-HH",
+          audioSrc: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+        }
       ]
-    }
+    };
     this.displaySound = this.displaySound.bind(this);
-    this.toggleActive = this.toggleActive.bind(this)
+    this.toggleActive = this.toggleActive.bind(this);
   }
 
-  displaySound(sound){
+  displaySound(sound) {
     this.setState({
-      display : sound
-    })
+      display: sound
+    });
   }
 
-  toggleActive(){
+  toggleActive() {
     this.setState({
-      active : !this.state.active
-    })
+      active: !this.state.active
+    });
   }
 
   render() {
     const onStyle = {
-      color: this.state.active ? '#d4d4d4' : '#374c50',
-      borderRight : '2px solid #d4d4d4',
-    }
+      color: this.state.active ? "#d4d4d4" : "#374c50",
+      borderRight: "2px solid #d4d4d4"
+    };
     const offStyle = {
-      color: this.state.active ? '#374c50' : '#d4d4d4'
-    }
+      color: this.state.active ? "#374c50" : "#d4d4d4"
+    };
     return (
       <div className="App">
-        <div className='drum-machine'>
-          <button onClick={this.toggleActive} className='toggleActive'><span style={onStyle}>on</span><span style={offStyle}>off</span></button>
-          <p id='display'>{this.state.display}</p>
-          <Buttons btnsData = {this.state.btnsData} display={this.displaySound} activeState={this.state.active} />
+        <div className="drum-machine">
+          <button onClick={this.toggleActive} className="toggleActive">
+            <span style={onStyle}>on</span>
+            <span style={offStyle}>off</span>
+          </button>
+          <p id="display">{this.state.display}</p>
+          <Buttons
+            btnsData={this.state.btnsData}
+            display={this.displaySound}
+            activeState={this.state.active}
+          />
         </div>
       </div>
     );
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,89 +1,96 @@
-
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Buttons extends Component {
   // play sounds function
-  playAudio = (audio) => {
+  playAudio = audio => {
     // setting audio time to 0 for multiple fast clicks
-    audio.currentTime = 0
+    audio.currentTime = 0;
     // play audio
-    audio.play()
-  }
+    audio.play();
+  };
   // onClick function
-  handleClick = (e) => {
+  handleClick = e => {
     // check if app is active or not
-    if(this.props.activeState){
+    if (this.props.activeState) {
       // get audio child element from clicked button
-      let audio = e.target.children[0]
+      let audio = e.target.children[0];
       // call play sound function
-      this.playAudio(audio)
+      this.playAudio(audio);
       // pass button id to display prop
-      this.props.display(e.target.id)
+      this.props.display(e.target.id);
       // remove display value after 2 sec. by sending empty string through display prop
-      setTimeout( () => {
-        this.props.display("")
-      },2000)
+      setTimeout(() => {
+        this.props.display("");
+      }, 2000);
     }
-  }
-  
-  componentDidMount(){
+  };
+
+  componentDidMount() {
     // add event listener for keyboard clicks
-    document.addEventListener('keydown', (e) => {
+    document.addEventListener("keydown", e => {
       // check if app is active or not
-      if(this.props.activeState){
+      if (this.props.activeState) {
         // add event listener only if the clicked button is within a set of buttons
-        if ([81,87,69,65,83,68,90,88,67].indexOf(e.keyCode) !== -1){
+        if ([81, 87, 69, 65, 83, 68, 90, 88, 67].indexOf(e.keyCode) !== -1) {
           //get audio with id equals to matching letter clicked on keyboard
-          let audio = document.querySelector(`#${String.fromCharCode(e.keyCode)}`)
-          this.playAudio(audio)
+          let audio = document.querySelector(
+            `#${String.fromCharCode(e.keyCode)}`
+          );
+          this.playAudio(audio);
           // get sound from button element which is a parent of audio element with id equals to matching letter clicked on keyboard
-          let display =  document.querySelector(`#${String.fromCharCode(e.keyCode)}`).parentElement.id
+          let display = document.querySelector(
+            `#${String.fromCharCode(e.keyCode)}`
+          ).parentElement.id;
           // pass sound to display prop
-          this.props.display(display)
+          this.props.display(display);
           // remove display value after 2 sec. by sending empty string through display prop
-          setTimeout( () => {
-            this.props.display("")
-          },2000)
+          setTimeout(() => {
+            this.props.display("");
+          }, 2000);
         }
       }
-    })
+    });
   }
 
   render() {
     const btnsContainer = {
-      display : 'grid',
-      gridTemplateColumns : 'repeat(3 , 1fr)',
-      gridGap : '5px',
-    }
+      display: "grid",
+      gridTemplateColumns: "repeat(3 , 1fr)",
+      gridGap: "5px"
+    };
     const btnStyle = {
-      minWidth: '4rem',
-      minHeight: '4rem',
-      border:'5px outset #5a6a6d',
-      color: '#d4d4d4',
-      background : '#3e565b',
-      fontSize : '1.5rem',
-      fontWeight: '700',
-    }
+      minWidth: "4rem",
+      minHeight: "4rem",
+      border: "5px outset #5a6a6d",
+      color: "#d4d4d4",
+      background: "#3e565b",
+      fontSize: "1.5rem",
+      fontWeight: "700"
+    };
     return (
-      <div style={btnsContainer} >
+      <div style={btnsContainer}>
         {this.props.btnsData.map(btn => {
-            return (
-              <button style={btnStyle} key={btn.key} className='drum-pad' id={btn.sound} onClick={this.handleClick} >
-                {btn.id}
-                <audio src={btn.audioSrc} className='clip' id={btn.id} />
-              </button>
-            )
-          }
-        )}
+          return (
+            <button
+              style={btnStyle}
+              key={btn.key}
+              className="drum-pad"
+              id={btn.sound}
+              onClick={this.handleClick}
+            >
+              {btn.id}
+              <audio src={btn.audioSrc} className="clip" id={btn.id} />
+            </button>
+          );
+        })}
       </div>
     );
   }
 }
 
 Buttons.propTypes = {
-  btnsData : PropTypes.array.isRequired
+  btnsData: PropTypes.array.isRequired
 };
 
-export default Buttons
-
+export default Buttons;
